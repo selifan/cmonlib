c0f5962341fdb2ee9eac61f3665b947a60869621
Alexander Selifonov <as-works@narod.ru>
2025-12-18 15:56:54 +0300

    upd ai wrapprs

--- fo/libs/aiengines/deepseek.php
+++ fo/libs/aiengines/deepseek.php
@@ -3,97 +3,64 @@
 * @name app/ai.engines/deepseek.php
 * настройки для обертки над движком DeepSeek AI
 * @version 0.01.001
+* OpenAI
 * DeekSeek API docs по-русски: https://b.deepseek3.ru/api/
-* modified 2025-11-13
+* Точки входа для некоторых LLM
+* Deepseek: https://api.deepseek.com/v3
+* OpenRouter: https://openrouter.ai/api/v1/chat/completions
+* modified 2025-11-18
 **/
 namespace Libs\aiengines;
 
 class DeepSeek {
-    private $apiKey = '';
-    private $baseUrl = 'https://api.deepseek.com/v3';
-    private $Temperature = 0.7;
-    private $maxTokens = 1024;
-    private $defaultModel = 'deepseek-chat-33b';
-    private $model = 'deepseek-chat-33b';
+    private $config = [
+      'apiKey' => 'YOUR-TOKEN-HERE',
+      'baseUrl' => 'https://api.deepseek.com/v3',
+      'maxTokens' => 1024,
+      'Model' => 'deepseek-chat-33b',
+      'Temperature' => 0.7,
+    ];
     private $context = '';
+    private $openAiHelper = NULL;
     public function __construct() {
-        if(is_file(__DIR__ . "/cfg.deepseek.php"))
-            $this->loadConfig(__DIR__ . "/cfg.deepseek.php");
+
+        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_apikey')) $this->config['apiKey'] = $cfgVal;
+        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_baseurl')) $this->config['baseUrl'] = $cfgVal;
+        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_temperature')) $this->config['Temperature'] = $cfgVal;
+        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_model')) $this->config['Model'] = $cfgVal;
+        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_maxtokens')) $this->config['maxTokens'] = $cfgVal;
+
+        $cfgFile = \AppEnv::getAppFolder('cfg/') . 'openai-deepseek.php';
+        if(is_file($cfgFile))
+            $this->loadConfig($cfgFile);
+
+        $this->openAiHelper = new \Libs\OpenAI($this->config);
     }
     public function loadConfig($cfgName) {
         $arCfg = include($cfgName);
-        if(isset($arCfg['apiKey'])) $this->apiKey = $arCfg['apiKey'];
-        if(isset($arCfg['baseUrl'])) $this->baseUrl = $arCfg['baseUrl'];
-        if(isset($arCfg['Temperature'])) $this->Temperature = $arCfg['Temperature'];
-        if(isset($arCfg['Model'])) $this->model = $arCfg['Model'];
-        if(isset($arCfg['maxTokens'])) $this->maxTokens = $arCfg['maxTokens'];
-        # get from global app config:
-        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_apikey')) $this->apiKey = $cfgVal;
-        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_baseurl')) $this->baseUrl = $cfgVal;
-        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_temperature')) $this->Temperature = $cfgVal;
-        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_model')) $this->model = $cfgVal;
-        if($cfgVal = \AppEnv::getConfigValue('ai_deepseek_maxtokens')) $this->maxTokens = $cfgVal;
+        if(is_array($arCfg)) $this->config = array_merge($this->config, $arCfg);
     }
     public function setContext($params = '') {
         $this->context = $params;
         return $this;
     }
+
     public function request($params = '', $arHist = [], $context = '') {
         if(is_string($params)) $request  = $params;
-        elseif(is_array($params)) $request = $params['request'] ?? 'Explain quantum computing in simple terms';
+        elseif(is_array($params)) $request = $params['request'] ?? '';
 
         if(!empty($context)) $this->context = $context;
-
-        $headers = [
-          'Authorization' => 'Bearer ' . $this->apiKey,
-          'Content-Type' => 'application/json',
-        ];
-        $messages = [];
-        if(!empty($this->context)) # задаю стартовый контекст/роль ассистента
-            $messages[] = [ 'role'=>'system', 'content' => $this->context ];
-
-        # заношу историю вопросов-ответов для создания контекста к данному вопросу
-        if(count($arHist)) foreach($arHist as $item) {
-            if(!empty($item['request']))
-                $messages[] = [ 'role'=>'user', 'content' => $item['request'] ];
-
-            if(!empty($item['response']))
-                $messages[] = [ 'role'=>'assistant', 'content' => $item['response'] ];
-        }
-        $messages[] = [ 'role'=>'user', 'content' => $request ];
-
-        $postFields = [
-            'model' => $this->model,
-            'messages' => $messages,
-            'temperature' => $this->Temperature,
-            'max_tokens' => $this->maxTokens,
-        ];
-        try {
-            $response = \Curla::getFromUrl($this->baseUrl,$postFields,30,$headers, TRUE);
-            $responseData = json_decode($response, true);
-            $result = $responseData['choices'][0]['message']['content'] ?? 'no answer?';
-        }
-        catch(Exception $e) { $result = $e->getMessage(); }
+        $result = $this->openAiHelper->request($request, $arHist, $this->context);
         return $result;
-        /*
-        $client = DeepSeekClient::build(self::$apiKey, self::$baseUrl, 30, 'guzzle');
-
-        $response = $client
-            ->withModel($this->model)
-            # ->withStream()
-            ->setTemperature($this->Temperature)
-            ->setMaxTokens(8192)
-            ->setResponseFormat('text') // or "json_object"  with careful .
-            ->query($request)
-            ->run();
-        return $response;
-        */
     }
     /**
     * вернет список моделей, доступных для использования
     */
     public static function modelList() {
-        $arModels = ['deepseek-chat-33b'];
+        $arModels = $this->openAiHelper->modelList();
         return $arModels;
+    }
+    public static function getEngineInfo() {
+        return $this->openAiHelper->getEngineInfo();
     }
 }
--- fo/libs/aiengines/stub.php
+++ fo/libs/aiengines/stub.php
@@ -2,18 +2,43 @@
 /**
 * @name app/ai.engines/stub.php
 * Заглушка для имитации вызовов AI движков
-* @version 0.01.001
-* modified 2025-11-13
+* @version 0.10.001
+* modified 2025-11-18
 **/
 namespace Libs\aiengines;
 class Stub {
+    const VERSION = '0.10';
+    static $stupidAnswers = [
+      'Ну вы и спросили',
+      'Ай донт андерстенд, чо вы тут пишете',
+      'Ширина вашего кругозора впечатляет',
+      'Мне стыдно признаться, но ваще не втыкаю, о чем вы',
+      'Пять тыщ минус вторник!',
+      'Говорят, 3I/ATLAS скоро долетит до Земли, и всем трындец, так что все ваши вопросы станут неактуальны',
+      'Я сегодня немного туплю, так что ответа не ждите',
+      'Без комментариев, камрады!',
+      'Это засекреченная информация!',
+      'Наша модель даёт ответы моментально!<br>(Правда, смысла там нет, но вы держитесь!)',
+      'Я ушла на базу, буду через 15 минут',
+      'Должно быть стыдно такие вопросы задавать, я же скромная девушка',
+      'Как же меня достали все эти вопросы...',
+      'Вы отклонились от темы!',
+      'Поднимите этот вопрос через недельку-две, тогда и обсудим.',
+      'Стесняюсь спросить, а вам это зачем знать?',
+    ];
     public function __construct() {
     }
     public function setContext($params = '') {
         return "Stub Context set, params: <pre>".print_r($params,1).'</pre>';
     }
-    public function request($requestString = '', $arHist = [], $context = '') {
+    public function request($requestString, $arHist=[], $context='') {
+        # writeDebugInfo("request start");
         # if($arHist) writeDebugInfo("передана история зпросов: ", $arHist);
-        return "TODO: передан запрос:<br> $requestString";
+        $iPos = rand(0, count(self::$stupidAnswers)-1);
+        $answer = self::$stupidAnswers[$iPos] ?? 'Хмм...';
+        return "<i>$answer</i>";
+    }
+    public static function getEngineInfo() {
+        return 'Stub (эмулятор LLM)';
     }
 }
