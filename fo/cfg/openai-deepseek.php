e0923eb65627333bc818ca1c962704cb949af47d
Alexander Selifonov <as-works@narod.ru>
2025-12-18 15:52:07 +0300

    Create openai-deepseek.php

---
+++ fo/cfg/openai-deepseek.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @name cfg/openai-deepseek.php
+* Настройки для работы с DeepSeek
+* https://api.deepseek.com/v3
+* https://api.deepseek.com/chat/completions
+* Пример: https://b.deepseek3.ru/create-chat-completion/
+*/
+return [
+  'baseUrl' => 'https://api.deepseek.com/v3', # URL вызова AI провайдера
+  'apiKey' => 'YOUR_API_KEY', # deepsek API key
+  'Temperature' => 0.5,
+  'Model' => 'deepseek-chat-33b', # gpt-4-turbo
+  'maxTokens' => 4096,
+];
